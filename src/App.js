50417a6ccd1921ee4ee080b855fe363c8109788d
MBA2022 <mohdbm.amr@gmail.com>
2024-08-27 11:21:34 +0300

    initializing..

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useEffect } from 'react';
 import './App.css';
+import logo from './mba.svg'; // Make sure you have a logo.svg file in your src folder
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      const particle = document.createElement('div');
+      particle.className = 'particle';
+      document.body.appendChild(particle);
+      const size = Math.random() * 10 + 2;
+      particle.style.width = particle.style.height = `${size}px`;
+      
+      // Set the particle color to a pink or purple
+      const color = Math.random() > 0.5 ? 'rgba(255, 105, 180, 1)' : 'rgba(128, 0, 128, 1)';
+      particle.style.backgroundColor = color;
+
+      const posX = e.clientX - size / 2;
+      const posY = e.clientY - size / 2;
+      particle.style.left = `${posX}px`;
+      particle.style.top = `${posY}px`;
+
+      const deltaX = (Math.random() - 0.5) * 50;
+      const deltaY = (Math.random() - 0.5) * 50;
+      setTimeout(() => {
+        particle.style.opacity = '0';
+        particle.style.transform = `translate(${deltaX}px, ${deltaY}px)`;
+      }, 1);
+
+      setTimeout(() => {
+        particle.remove();
+      }, 800);
+    };
+
+    document.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      document.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
+    return (
+        <div className="App">
+          <title>MBA</title>
+            <div className="logo-circle">
+                <img src={logo} alt="MBA Logo" />
+            </div>
+        </div>
+    );
 }
 
 export default App;
